3450845e5f0218a571188b35ce004c1fed3a9837
Serhii <chuhaystyr@gmail.com>
2019-12-29 16:42:15 +0200

    add components ant rout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainPageComponent } from './main-page/main-page.component';
+import { ArticlesPageComponent } from './articles-page/articles-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainPageComponent},
+  { path: 'articles', component: ArticlesPageComponent},
+  //{ path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,38 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { ArticlesPageComponent } from './articles-page/articles-page.component';
+import { FooterComponent } from './footer/footer.component';
+import { ArticleComponent } from './articles-page/article/article.component';
+import { EventsPageComponent } from './events-page/events-page.component';
+import { EventsTabComponent } from './events-page/events-tab/events-tab.component';
+import { GalleryPageComponent } from './gallery-page/gallery-page.component';
+import { MenuComponent } from './header/menu/menu.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatButtonModule} from '@angular/material/button';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainPageComponent,
+    ArticlesPageComponent,
+    FooterComponent,
+    ArticleComponent,
+    EventsPageComponent,
+    EventsTabComponent,
+    GalleryPageComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]

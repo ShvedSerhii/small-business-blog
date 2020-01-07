beb0e3a398d8d086d268dc5d87afdc9b47b1e625
Serhii <chuhaystyr@gmail.com>
2020-01-07 02:44:19 +0200

    add page not found

--- src/app/components/header/menu/menu.component.ts
+++ src/app/components/header/menu/menu.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent implements OnInit {
-  public homeActive = 'active';
+  public homeActive = '';
   public eventsActive = '';
   public galleryActive = '';
   public articlesActive = '';

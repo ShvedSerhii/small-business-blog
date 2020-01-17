8510e02dd897a00a1703acc2a5799dab25b77166
Serhii <chuhaystyr@gmail.com>
2020-01-17 16:31:01 +0200

    add logout

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
-import { LoginModel } from './login.model';
-import LoginForm from './login.form';
-import { LoginService } from '../../services/login.service';
+import { Component } from "@angular/core";
+import { LoginModel } from "./login.model";
+import LoginForm from "./login.form";
+import { LoginService } from "../../services/login.service";
+import { CookiesService } from "../../services/cookies.service";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"]
 })
 export class LoginComponent {
   public success: any;
@@ -15,17 +16,28 @@ export class LoginComponent {
   data: any;
   public email: any;
   public password: any;
-  constructor(private loginService: LoginService) {
+  constructor(
+    private loginService: LoginService,
+    private cookie: CookiesService
+  ) {
     this.model = new LoginModel();
     this.form = new LoginForm(this.model);
     this.success = {};
-    this.email = this.form.formGroup.get('email');
-    this.password = this.form.formGroup.get('password');
+    this.email = this.form.formGroup.get("email");
+    this.password = this.form.formGroup.get("password");
+  }
+
+  isAutorization() {
+    if (!this.cookie.getCookie("token")) return true;
+    return false;
+  }
+  logout() {
+    this.cookie.deleteCookie("token");
   }
 
   onSubmit(form) {
     this.loginService.loginUser(data => {
-      console.log('data', data);
+      console.log("data", data);
       this.data = data;
       this.loginService.setData(data);
       this.email.reset();

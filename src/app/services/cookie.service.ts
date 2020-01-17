a41952d36f947cf757e369efb07436426a13b898
Serhii <chuhaystyr@gmail.com>
2020-01-17 02:08:48 +0200

    add cookie

--- src/app/services/cookie.service.ts
+++ src/app/services/cookie.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
 
+import { CookieService } from 'ngx-cookie-service';
+
 @Injectable({
   providedIn: 'root'
 })
-export class CookieService {
+export class CookiesService {
 
-  constructor() { }
+  constructor(
+    private cookieService: CookieService
+  ) { }
+
+  setCookie (key: string, value: string, expires: number | Date = 365, secure: boolean = true) {
+    this.cookieService.set(key, value, expires, secure)
+  }
+
+  getCookie (key: string) {
+    this.cookieService.get(key)
+  }
+
+  deleteCookie (key: string) {
+    this.cookieService.delete(key)
+  }
 }

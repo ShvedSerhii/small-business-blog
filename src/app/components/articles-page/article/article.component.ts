df78951f7bb53be2781a16e48b78e30baf713712
Serhii <chuhaystyr@gmail.com>
2020-01-22 22:40:21 +0200

    add logout

--- src/app/components/articles-page/article/article.component.ts
+++ src/app/components/articles-page/article/article.component.ts
@@ -1,14 +1,14 @@
-import { Component, Input } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { EditArticleComponent } from '../edit-article/edit-article.component';
-import { EditData } from '../edit-article/edit-data.model';
-import { ArticlesControllerService } from 'src/app/services/articles-controller.service';
-
+import { Component, Input } from "@angular/core";
+import { MatDialog } from "@angular/material/dialog";
+import { EditArticleComponent } from "../edit-article/edit-article.component";
+import { EditData } from "../edit-article/edit-data.model";
+import { ArticlesControllerService } from "src/app/services/articles-controller.service";
+import { CookiesService } from "src/app/services/cookies.service";
 
 @Component({
-  selector: 'app-article',
-  templateUrl: './article.component.html',
-  styleUrls: ['./article.component.scss']
+  selector: "app-article",
+  templateUrl: "./article.component.html",
+  styleUrls: ["./article.component.scss"]
 })
 export class ArticleComponent {
   @Input() title: string;
@@ -20,20 +20,30 @@ export class ArticleComponent {
 
   public data: EditData;
 
-  constructor(public dialog: MatDialog, public articlesController: ArticlesControllerService) {
+  constructor(
+    public dialog: MatDialog,
+    public articlesController: ArticlesControllerService,
+    private cookie: CookiesService
+  ) {
     this.data = new EditData();
+  }
+
+  isAuthorization() {
+    if (this.cookie.getCookie("token")) {
+      return true;
+    }
+    return false;
   }
 
   openDialog(): void {
     const dialogRef = this.dialog.open(EditArticleComponent, {
-      width: '600px',
-      data: this.articlesController.getCurrentArticle(this.index) 
+      width: "600px",
+      data: this.articlesController.getCurrentArticle(this.index)
     });
 
     dialogRef.afterClosed().subscribe(result => {
       if (result.title && result.date && result.author && result.content)
-        this.articlesController.updateCurrentArticle(result, this.index); 
+        this.articlesController.updateCurrentArticle(result, this.index);
     });
   }
 }
- 
--- src/app/components/articles-page/articles-page.component.ts
+++ src/app/components/articles-page/articles-page.component.ts
@@ -1,28 +1,40 @@
-import { Component } from '@angular/core';
-import { ArticleModel } from './article/article.model';
-import { MatDialog } from '@angular/material/dialog';
-import { EditArticleComponent } from './edit-article/edit-article.component';
-import { EditData } from './edit-article/edit-data.model';
-import { ArticlesControllerService } from 'src/app/services/articles-controller.service';
+import { Component } from "@angular/core";
+import { ArticleModel } from "./article/article.model";
+import { MatDialog } from "@angular/material/dialog";
+import { EditArticleComponent } from "./edit-article/edit-article.component";
+import { EditData } from "./edit-article/edit-data.model";
+import { ArticlesControllerService } from "src/app/services/articles-controller.service";
+import { CookiesService } from "src/app/services/cookies.service";
 
 @Component({
-  selector: 'app-articles-page',
-  templateUrl: './articles-page.component.html',
-  styleUrls: ['./articles-page.component.scss']
+  selector: "app-articles-page",
+  templateUrl: "./articles-page.component.html",
+  styleUrls: ["./articles-page.component.scss"]
 })
 export class ArticlesPageComponent {
   public articles: ArticleModel[];
   public data: EditData;
 
-  constructor(public dialog: MatDialog, public articlesController: ArticlesControllerService) {
+  constructor(
+    public dialog: MatDialog,
+    public articlesController: ArticlesControllerService,
+    private cookie: CookiesService
+  ) {
     this.data = new EditData();
     this.articles = articlesController.getArticles();
   }
 
+  isAuthorization() {
+    if (this.cookie.getCookie("token")) {
+      return true;
+    }
+    return false;
+  }
+
   openDialog(): void {
     const dialogRef = this.dialog.open(EditArticleComponent, {
-      width: '600px',
-      data: this.data 
+      width: "600px",
+      data: this.data
     });
 
     dialogRef.afterClosed().subscribe(result => {

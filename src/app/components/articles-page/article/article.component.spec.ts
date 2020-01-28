338676032cd66edb420bd3ca6a961856e165f116
Serhii <chuhaystyr@gmail.com>
2020-01-28 03:05:57 +0200

    add test

--- src/app/components/articles-page/article/article.component.spec.ts
+++ src/app/components/articles-page/article/article.component.spec.ts
@@ -1,14 +1,31 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ArticleComponent } from './article.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ArticlesControllerService } from 'src/app/services/articles-controller/articles-controller.service';
 
 describe('ArticleComponent', () => {
   let component: ArticleComponent;
   let fixture: ComponentFixture<ArticleComponent>;
 
+  const matDialogStub = {
+    open: () => {}
+  }
+
+  const articlesControllerServiceStub = {
+    getCurrentArticle: () => {},
+    updateCurrentArticle: () => {} 
+  }
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ArticleComponent]
+      declarations: [ArticleComponent],
+      providers: [
+        {provide: ArticlesControllerService, useValue: articlesControllerServiceStub},
+        {provide: MatDialog, useValue: matDialogStub}
+      ],
+      schemas: [ NO_ERRORS_SCHEMA ] 
     }).compileComponents();
   }));
 

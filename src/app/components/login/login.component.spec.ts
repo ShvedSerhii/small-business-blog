8991caa07a1d69f4c8ad344a1eec5b93b101af15
Serhii <chuhaystyr@gmail.com>
2020-01-30 22:51:29 +0200

    add tests to login component

--- src/app/components/login/login.component.spec.ts
+++ src/app/components/login/login.component.spec.ts
@@ -1,17 +1,21 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { DebugElement } from "@angular/core";
+import { BrowserModule, By } from "@angular/platform-browser";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { LoginComponent } from "./login.component";
+import { LoginService } from "src/app/services/login/login.service";
+import { CookiesService } from "src/app/services/cookies/cookies.service";
+import { Router } from "@angular/router";
+import { BusService } from "src/app/services/bus/bus.service";
+import { NO_ERRORS_SCHEMA } from "@angular/core";
 
-import { LoginComponent } from './login.component';
-import { LoginService } from 'src/app/services/login/login.service';
-import { CookiesService } from 'src/app/services/cookies/cookies.service';
-import { Router } from '@angular/router';
-import { BusService } from 'src/app/services/bus/bus.service';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
-
-describe('LoginComponent', () => {
+describe("LoginComponent", () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let de: DebugElement;
+  let el: HTMLElement;
 
-  const loginServiceStub = {  };
+  const loginServiceStub = {};
   const cookieStub = {
     deleteCookie: () => {}
   };
@@ -24,25 +28,59 @@ describe('LoginComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ],
+      declarations: [LoginComponent],
+      imports: [BrowserModule, FormsModule, ReactiveFormsModule],
       providers: [
-        {provide: LoginService, useValue: loginServiceStub},
-        {provide: CookiesService, useValue: cookieStub},
-        {provide: Router, useValue: routerStub},
-        {provide: BusService, useValue: busStub},
+        { provide: LoginService, useValue: loginServiceStub },
+        { provide: CookiesService, useValue: cookieStub },
+        { provide: Router, useValue: routerStub },
+        { provide: BusService, useValue: busStub }
       ],
       schemas: [NO_ERRORS_SCHEMA]
-    })
-    .compileComponents();
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
+    de = fixture.debugElement.query(By.css("form"));
+    el = de.nativeElement;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
+
+  it("form invalid when empty", () => {
+    expect(component.form.formGroup.valid).toBeFalsy();
+  });
+
+  it('form invalid', () => {
+    component.form.formGroup.controls['email'].setValue('test');
+    component.form.formGroup.controls['password'].setValue('test');
+    expect(component.form.formGroup.valid).toBeFalsy();
+  });
+
+  it('form valid', () => {
+    component.form.formGroup.controls['email'].setValue('test@test.com');
+    component.form.formGroup.controls['password'].setValue('password');
+    expect(component.form.formGroup.valid).toBeTruthy();
+  });
+
+  it('should call submit method', () => {
+    fixture.detectChanges();
+    spyOn<any>(component, 'onSubmit');
+    el = fixture.debugElement.query(By.css('button')).nativeElement;
+    el.click();
+    expect(component.onSubmit).toHaveBeenCalledTimes(0);
+  });
+
+  it('should call logout method', () => {
+    spyOn(component, 'logout');
+    component.logout();
+    expect(component.logout).toHaveBeenCalled();
+});
+
+
 });
